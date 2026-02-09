6f40e527ef26db895564677a772d2ea9aac742e7
MiroCillik <mcillik@gmail.com>
2026-02-09 13:10:26 +0100

    local secrets 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,10 @@ st.set_page_config(
 )
 
 st.write(f"Environment: {os.environ.get('environment', 'not set')}")
-st.write("Secrets:", dict(st.secrets))
+try:
+    st.write("Secrets:", dict(st.secrets))
+except Exception as e:
+    st.write(f"Secrets error: {e}")
 
 input_dir = '/data/in/tables/'
 client = Client(st.secrets.kbc_url, st.secrets.kbc_token)
